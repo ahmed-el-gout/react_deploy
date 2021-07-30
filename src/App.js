56efe998e41e90ddae9b247b9042a8b374077269
ahmed el gout <ahmedelgout7@gmail.com>
2021-07-30 13:09:52 +0100

    je m'apple

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          bonjour tout le monde
+          je m'apple ahmed je suis developer web junior
         </p>
       </header>
     </div>
