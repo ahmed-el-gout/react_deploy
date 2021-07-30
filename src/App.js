105fbbdcf97bdce2a657fa22c5a62cec15e5ab4a
ahmed el gout <ahmedelgout7@gmail.com>
2021-07-30 13:36:07 +0100

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          je m'apple ahmed je suis developer web junior
+          test
         </p>
       </header>
     </div>
